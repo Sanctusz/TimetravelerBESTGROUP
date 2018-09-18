8f722f0767387c7de95fc4896d43e7a12c20af9f
Sanctusz <yohans89@gmail.com>
2018-09-18 15:16:39 +0000

    WE DID IT!!!

--- timetraveler.py
+++ timetraveler.py
@@ -1,30 +1,51 @@
 #news
-tilematrix = ["n","nes","es","ew","ws","n","ws","ns","n"]
-n = 0
-m = 0
+'''
+N = +1
+E = +3
+W = -3
+S = -1
+'''
+
+tilematrix = ["n","nes","es","n","ws","ew","n","ns","ws"]
+n = 0 #location of player
+m = 0 #
 diravailable = ''
 
-for i in tilematrix:
-    diravailable = ''
-    m = 0
-    for j in tilematrix[n]:
-        if j == "n":
-            diravailable = "You can travel: (N)orth"
-        elif j == "s":
-            if m == 0:
-                diravailable = "You can travel: (S)outh"
-            else:
-                diravailable += " or (South)"
-        elif j == "w":
-            if m == 0:
-                diravailable = "You can travel: (W)est"
-            else:
-                diravailable += " or (W)est"
-        elif j == "e":
-            if m == 0:
-                diravailable = "You can travel: (E)east"
-            else:
-                diravailable += " or (E)ast"
-        m += 1
-    n += 1
-    print(diravailable)
+while n != 6:
+    way = input("Direction: ")
+    if way in tilematrix[n]:
+        if way == "n":
+            n += 1
+        elif way == "e":
+            n += 3
+        elif way == "w":
+            n -= 3
+        elif way == "s":
+            n -= 1
+    if n == 6:
+        print("WINNER!")
+        break
+    for i in tilematrix:
+        diravailable = ''
+        m = 0
+        for j in tilematrix[n]:
+            if j == "n":
+                diravailable = "You can travel: (N)orth"
+            elif j == "s":
+                if m == 0:
+                    diravailable = "You can travel: (S)outh"
+                else:
+                    diravailable += " or (South)"
+            elif j == "w":
+                if m == 0:
+                    diravailable = "You can travel: (W)est"
+                else:
+                    diravailable += " or (W)est"
+            elif j == "e":
+                if m == 0:
+                    diravailable = "You can travel: (E)east"
+                else:
+                    diravailable += " or (E)ast"
+            m += 1
+        #n += 1
+        #print(diravailable)
