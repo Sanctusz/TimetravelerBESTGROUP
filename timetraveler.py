6b3cbc4c0553f29af6f19f2f251f024a81ae6b76
Sanctusz <yohans89@gmail.com>
2018-09-18 14:39:53 +0000

    repush
    fixing...

--- timetraveler.py
+++ timetraveler.py
@@ -1,46 +1,30 @@
-posx = 2
-posy = 3
-
-maxpos = 3
-minpos = 1
-#3,1 win tile
 #nswe
-tilematrix = ["n","nse","ne","we","sw","n","sw","ns","n"]
+tilematrix = ["n","nse","se","we","sw","n","sw","ns","n"]
 n = 0
 m = 0
 diravailable = ''
 
 for i in tilematrix:
-    print(n, tilematrix[n])
+    diravailable = ''
     for j in tilematrix[n]:
-        m += 1
         if j == "n":
             diravailable = "You can travel: (N)orth"
         elif j == "s":
-            if m == 1:
+            if m == 0:
                 diravailable = "You can travel: (S)outh"
             else:
                 diravailable += " or (South)"
         elif j == "w":
-            if m == 1:
+            if m == 0:
                 diravailable = "You can travel: (W)est"
             else:
                 diravailable += " or (W)est"
-        else:
-            if m == 1:
+        elif j == "e":
+            if m == 0:
                 diravailable = "You can travel: (E)east"
             else:
                 diravailable += " or (E)ast"
+        m += 1
     n += 1
     print(diravailable)
 
-'''
-if posx == 1 and posy == 1:
-    print(tilematrix[0])
-elif posx == 1 and posy == 2:
-    print(tilematrix[1])
-elif posx == 1 and posy == 3:
-    print(tilematrix[2])
-'''
-if posx == 3 and posy == 1:
-    print("winner!!")
