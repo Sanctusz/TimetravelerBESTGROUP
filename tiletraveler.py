ee83a4e39fd0247db758558cec235a944875af78
Spiritduck <38790717+Spiritduck@users.noreply.github.com>
2018-09-21 00:17:35 +0000

    Q&A

--- tiletraveler.py
+++ tiletraveler.py
@@ -1,5 +1,22 @@
 #news
 '''
+1. Which implementation was easier and why?
+    The latter one, tiletraveler.
+    Since it´s based on the first program. Making the second program was
+    easier since we already solved the problem, we just improved our solution.
+    One way to look at it is that the first program was a draft, an algorithm we could implement into our second program 
+
+2. Which implementation is more readable and why?
+    The second one, "tiletraveler" is a lot more readable because of the functions. 
+    By sectioning the program, it becomes less intimidating and not as much of a clutter. 
+    It can be hard for anyone other than the original programmers to read this kind of code since they would have to spend more time analyzing how it works. 
+
+3. Which problems in the first implementations were you able to solve with the latter implementation?
+    Beside the problem of overall readability, the new program makes it easier for us to make another similar program based on this one by using the functions. 
+    The program is also named correctly this time.
+
+'''
+'''
 N = +1
 E = +3
 W = -3
